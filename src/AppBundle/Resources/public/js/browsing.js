658d283e09c2f150803b16b2b097ac4eb283318f
Malah <malah@neuf.fr>
2016-12-20 11:21:43 +0100

    amélioration de la vue et des css, close #8

--- src/AppBundle/Resources/public/js/browsing.js
+++ src/AppBundle/Resources/public/js/browsing.js
@@ -4,12 +4,14 @@ window.addEventListener('scroll', function (e) {
     titleEl = document.getElementById('title');
     navbarEl = document.getElementById('navbar');
     returnEl = document.getElementById('return');
+    navSecEl = document.getElementById('navSec');
     var bottomNavbar = navbarEl.getBoundingClientRect().bottom;
+    var bottomNavSecEl = navSecEl.getBoundingClientRect().bottom;
     var topTitle = titleEl.getBoundingClientRect().top;
     if (topTitle < 0) {
         titleEl.classList.add('fixed');
         returnEl.classList.add('right');
-    } else if (bottomNavbar > 0) {
+    } else if (bottomNavbar > 0 || bottomNavSecEl > 0) {
         titleEl.classList.remove('fixed');
         returnEl.classList.remove('right');
     }
