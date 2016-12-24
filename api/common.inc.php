63678b6ea29e647ee9e0b903635cb4ffe9679d20
malah <malah@neuf.fr>
2016-12-24 16:36:46 +0100

    ajout du CORS

--- api/common.inc.php
+++ api/common.inc.php
@@ -2,7 +2,6 @@
 
 // Dependencies
 require_once dirname(__FILE__) . '/vendor/autoload.php';
-//require_once dirname(__FILE__) . '/Controller/document.php'; // un autoloader pourrait être plus intéerssant ...
 
 // Config
 require_once dirname(__FILE__) . '/config.inc.php';
@@ -10,6 +9,18 @@ require_once dirname(__FILE__) . '/config.inc.php';
 // Slim
 $app = new \Slim\App($config);
 require_once dirname(__FILE__) . '/routing.php';
+
+// CORS see https://www.slimframework.com/docs/cookbook/enable-cors.html
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
 
 // Doctrine
 use Doctrine\ORM\Tools\Setup;
