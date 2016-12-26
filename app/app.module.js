583cf8483718b9be2bd3bc7a54105ca1acd141fc
malah <malah@neuf.fr>
2016-12-26 16:11:37 +0100

    suppression des css font-awesome (passé en cdn), suppression du travail sur le router non fonctionnel

--- app/app.module.js
+++ app/app.module.js
@@ -1,11 +1,7 @@
 (function(app) {
-  var router = require("@angular/router");
   app.AppModule =
     ng.core.NgModule({
-      imports: [ ng.platformBrowser.BrowserModule, router.RouterModule.forRoot([
-                { path: '', name: 'News', component: NewsComponent,},
-                { path: 'document/:id', name: 'document', component: DocumentComponent }
-            ]) ],
+      imports: [ ng.platformBrowser.BrowserModule ],
       declarations: [ app.AppComponent, app.HeaderComponent, app.NavbarComponent, app.FooterComponent, app.ModalComponent ],
       bootstrap: [ app.AppComponent, app.HeaderComponent, app.NavbarComponent, app.FooterComponent, app.ModalComponent ]
     })
